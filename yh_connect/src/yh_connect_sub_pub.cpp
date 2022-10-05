636312a706f6603a551432999cd4a9565ded6ffe
jungwoonr <itswow74@gmail.com>
2022-10-05 17:22:11 +0900

    yh_connect homework7

---
+++ yh_connect/src/yh_connect_sub_pub.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+#include "std_msgs/Float32.h"
+
+ros::Publisher pub;
+
+void msgCallback(const std_msgs::Int32::ConstPtr& msg){
+    if(msg->data % 5 == 0){
+        std_msgs::Float32 float_msg;
+        float_msg.data = (float) msg->data / 3.0f;
+        pub.publish(msg);
+    }
+}
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "yh_connect_sub_pub");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe("yh_cn_int", 10, msgCallback);
+    pub = nh.advertise<std_msgs::Float32>("yh_cn_float", 10);
+
+    ros::spin();
+
+    return 0;
+}
+
