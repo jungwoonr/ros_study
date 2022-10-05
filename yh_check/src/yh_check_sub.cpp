0b3d52e010713a7e96c3fd1d268b321e50688b6d
jungwoonr <itswow74@gmail.com>
2022-10-05 12:59:07 +0900

    yh_check homework 6

--- yh_check/src/yh_check_sub.cpp
+++ yh_check/src/yh_check_sub.cpp
@@ -1,27 +1,31 @@
 #include "ros/ros.h"
 #include "yh_check/YhCheck.h"
 
-void msgCallback(const yh_check::YhCheck::ConstPtr& msg){
-    ROS_INFO("receive msg: %d", msg->stamp.sec); // msg의 stamp의 sec를 출력
-    ROS_INFO("receive msg: %d", msg->stamp.nsec); // msg의 stamp의 nsec를 출력
-    ROS_INFO("receive msg: %d", msg->data); // msg의 data를 출력
+bool distance = true;
+bool camera = true;
+
+void distanceCallback(const yh_check::YhCheck::ConstPtr& msg){
+    distance = msg->data;
+    if(distance && camera){
+        ROS_INFO("OK");
+    }
+}
+
+void cameraCallback(const yh_check::YhCheck::ConstPtr& msg){
+    camera = msg->data;
+    if(distance && camera){
+        ROS_INFO("OK");
+    }
 }
 
 int main(int argc, char** argv){
-    ros::init(argc, argv, "yh_check_sub"); // 노드 이름 초기화
-    ros::NodeHandle nh; // 노드 핸들 선언
+    ros::init(argc, argv, "yh_check_sub");
+    ros::NodeHandle nh;
 
+    ros::Subscriber sub_distance = nh.subscribe("check_distance", 10, distanceCallback);
+    ros::Subscriber sub_camera = nh.subscribe("check_camera", 10, cameraCallback);
 
-    // 서브크라이버 선언
-    // 패키지(msg_tutorial)의 메세지(Mymsg)를 이용한 서브크라이버(sub)를 선언한다.
-    // 토픽은 ("burger")이며, 서브크라이버큐(queue) 사이즈를 (30)으로 설정한다.
-    // 콜백 함수는 (msgCallback)이다.
-    ros::Subscriber sub = nh.subscribe("check_sub", 30, msgCallback);
-
-    // 콜백 함수 호출을 위한 함수, 메세지가 수신되기를 대기
-    // 수신되었을 경우 콜백 함수를 호출한다.
-    ros::spin(); // 대기하는 함수
+    ros::spin();
 
     return 0;
-
 }
